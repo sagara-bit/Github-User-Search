5b3673d7bde70d666be419b268d5681c10342a67
sagar kumar <12thboardssycollege@gmail.com>
2022-12-21 19:23:20 +0530

    first commit

---
+++ src/context/mockData.js/mockFollowers.js
@@ -0,0 +1,22 @@
+export default[
+    {
+      "login": "atul14357",
+      "id": 71604582,
+      "node_id": "MDQ6VXNlcjcxNjA0NTgy",
+      "avatar_url": "https://avatars.githubusercontent.com/u/71604582?v=4",
+      "gravatar_id": "",
+      "url": "https://api.github.com/users/atul14357",
+      "html_url": "https://github.com/atul14357",
+      "followers_url": "https://api.github.com/users/atul14357/followers",
+      "following_url": "https://api.github.com/users/atul14357/following{/other_user}",
+      "gists_url": "https://api.github.com/users/atul14357/gists{/gist_id}",
+      "starred_url": "https://api.github.com/users/atul14357/starred{/owner}{/repo}",
+      "subscriptions_url": "https://api.github.com/users/atul14357/subscriptions",
+      "organizations_url": "https://api.github.com/users/atul14357/orgs",
+      "repos_url": "https://api.github.com/users/atul14357/repos",
+      "events_url": "https://api.github.com/users/atul14357/events{/privacy}",
+      "received_events_url": "https://api.github.com/users/atul14357/received_events",
+      "type": "User",
+      "site_admin": false
+    }
+  ];
---
+++ src/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import { GithubProvider } from './context/context';
+import { Auth0Provider } from '@auth0/auth0-react';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+  <Auth0Provider 
+  domain="dev-avel0wgbsypnsgp0.us.auth0.com"
+    clientId="0N7fuvoBtIe19FCPQrkXA3fmzwNuW0dN"
+    redirectUri={window.location.origin}
+    cacheLocation="localstorage"
+  >
+  <GithubProvider>
+    <App />
+    </GithubProvider>
+    </Auth0Provider>
+  </React.StrictMode>
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
